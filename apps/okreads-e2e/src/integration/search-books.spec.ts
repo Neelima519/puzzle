6b4b9e49acfd7d1c9aa615b9f3e1d0c90d9c425f
chitrapillai12 <chitra.pillai12@gmail.com>
2021-11-15 09:36:35 +0530

    task 2

--- apps/okreads-e2e/src/integration/search-books.spec.ts
+++ apps/okreads-e2e/src/integration/search-books.spec.ts
@@ -2,7 +2,7 @@ describe('When: Use the search feature', () => {
   before(() => {
     cy.startAt('/');
   });
-
+  describe('should be able to search books by title', () => {
   it('Then: I should be able to search books by title', () => {
     cy.get('input[type="search"]').type('javascript');
 
@@ -10,8 +10,13 @@ describe('When: Use the search feature', () => {
 
     cy.get('[data-testing="book-item"]').should('have.length.greaterThan', 1);
   });
+})
+describe('should see search results as I am typing', () => {
+  it('Then: I should see search results as I am typing', () => {
+    cy.get('input[type="search"]').type('javascript');
 
-  // it('Then: I should see search results as I am typing', () => {
-  //   // TODO: Implement this test!
-  // });
+    cy.get('form').submit();
+
+    cy.get('[data-testing="book-item"]').should('have.length.greaterThan', 1);  });
+});
 });
--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -9,6 +9,7 @@ import {
 } from '@tmo/books/data-access';
 import { FormBuilder } from '@angular/forms';
 import { Book } from '@tmo/shared/models';
+import { Subject } from 'rxjs/internal/Subject';
 
 @Component({
   selector: 'tmo-book-search',
@@ -17,7 +18,6 @@ import { Book } from '@tmo/shared/models';
 })
 export class BookSearchComponent implements OnInit {
   books: ReadingListBook[];
-
   searchForm = this.fb.group({
     term: ''
   });
@@ -36,6 +36,10 @@ export class BookSearchComponent implements OnInit {
       this.books = books;
     });
   }
+
+  searchBookList() {
+    this.searchBooks();
+    }
 
   formatDate(date: void | string) {
     return date
